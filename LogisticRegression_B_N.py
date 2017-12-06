955bc920c8ae0b10cc61aa5f31def4cca58974b0
ZheyuWalker <jzyinzju@gmail.com>
2017-12-06 20:03:30 +0900

    Update LogisticRegression_B_N.py

--- LogisticRegression_B_N.py
+++ LogisticRegression_B_N.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
 
+
+### Logistic Regression Algorithm
+
+
+def fit(X_train, Y_train):
+  '''
+  Arguments:
+  X_train: input data, a numpy array of shape (n_x, m)
+  Y_train:  
+  
+  Returns:
+  W: weights vector, a numpy array of shape (1, n_x)
+  b: bias, a real number
+  '''
+
+def predict(X, W, b):
+  '''
+  Arguments:
+  X: test input data, a numpy array of shape(n_x, m)
+  W: optimized weights, a numpy array of shape (1, n_x)
+  b: optimized bias, a real number
+  
+  Returns:
+  Yhat: prediction of test input data X, a numpy array of shape (1, m)
+  accuracy: 
+  '''
